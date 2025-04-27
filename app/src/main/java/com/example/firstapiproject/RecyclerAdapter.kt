3d60496714a2fdcfc298ea0d4812d1867a4269ae
rami <ramisedra@gmail.com>
2025-04-27 17:41:11 +0300

    done by doing all of the task by also putting the picture into the player profile fragment (this one with  circle photo in the recycler view)

--- app/src/main/java/com/example/firstapiproject/RecyclerAdapter.kt
+++ app/src/main/java/com/example/firstapiproject/RecyclerAdapter.kt
@@ -32,41 +32,42 @@
 
             holder.binding.firstName.text = player.first_name
             holder.binding.lastName.text = player.last_name
-            val requestOptions = RequestOptions()
-            val roundedCornersOption = requestOptions.transform(CenterCrop(), RoundedCorners(20))
-            Glide.with(holder.itemView.context)
-                .load(player.imageUrl)
-                .apply (roundedCornersOption)
-                .error(holder.itemView.context.getDrawable(R.mipmap.ic_launcher_round))
-                .into(holder.binding.ivImage)
+    /*val requestOptions = RequestOptions()
+    val roundedCornersOption = requestOptions.transform(CenterCrop(), RoundedCorners(20))*/
+    Glide.with(holder.itemView.context)
+        .load(player.imageUrl)
+        //.apply (roundedCornersOption)
+        .circleCrop()
+        .error(holder.itemView.context.getDrawable(R.mipmap.ic_launcher_round))
+        .into(holder.binding.ivImage)
 
-            val weight = player.weight_pounds?.toString() ?: "-"
-            val jerseyNumber = player.jersey_number ?: "-"
-            val college = player.college ?: "-"
-            val country = player.country ?: "-"
-            val draftYear = player.draft_year ?: "-"
-            val draftRound = player.draft_round ?: "-"
-            val draftNumber = player.draft_number ?: "-"
-            val imageUrl = player.imageUrl ?: "-"
+    val weight = player.weight_pounds?.toString() ?: "-"
+    val jerseyNumber = player.jersey_number ?: "-"
+    val college = player.college ?: "-"
+    val country = player.country ?: "-"
+    val draftYear = player.draft_year ?: "-"
+    val draftRound = player.draft_round ?: "-"
+    val draftNumber = player.draft_number ?: "-"
+    val imageUrl = player.imageUrl ?: "-"
 
-            holder.itemView.setOnClickListener {
-                val bundle = Bundle().apply {
-                    putString("first_name", player.first_name)
-                    putString("last_name", player.last_name)
-                    putString("position", player.position)
-                    putString("weight", weight)
-                    putString("jersey_number", jerseyNumber)
-                    putString("college", college)
-                    putString("country", country)
-                    putString("draft_year", draftYear.toString())
-                    putString("draft_round", draftRound.toString())
-                    putString("draft_number", draftNumber.toString())
-                    putString("image_url", player.imageUrl)
-                }
-
-              playersListener(players[position])
-            }
+    holder.itemView.setOnClickListener {
+        val bundle = Bundle().apply {
+            putString("first_name", player.first_name)
+            putString("last_name", player.last_name)
+            putString("position", player.position)
+            putString("weight", weight)
+            putString("jersey_number", jerseyNumber)
+            putString("college", college)
+            putString("country", country)
+            putString("draft_year", draftYear.toString())
+            putString("draft_round", draftRound.toString())
+            putString("draft_number", draftNumber.toString())
+            putString("image_url", player.imageUrl)
         }
 
-        override fun getItemCount() = players.size
+      playersListener(players[position])
     }
+}
+
+override fun getItemCount() = players.size
+}
